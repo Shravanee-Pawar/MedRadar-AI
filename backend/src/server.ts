b1913e076f9695e4cec4e0ea34ea1f1b1dab3ed5
Aparnakarkhele <aparnakarkhele@gmail.com>
2026-08-21 01:16:12 +0530

    Connect backend with MongoDB

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import app from './app.js';
-
+import { connectDB } from "./db.js";
 const PORT = process.env.PORT || 5000;
-
+connectDB().then(() => {
 app.listen(PORT, () => {
   console.log(`🚀 MedRadar AI Backend running on port ${PORT}`);
   console.log(`📡 Health Check URL: http://localhost:${PORT}/health`);
+});
 });
